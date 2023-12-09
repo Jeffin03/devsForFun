b6b0dddac478cb5639d10499c458b3069afc0ac4
Jeffin03 <jeffyissac2203@gmail.com>
2023-12-09 15:20:33 +0530

    update

--- Green_Screen/script.js
+++ Green_Screen/script.js
@@ -2,6 +2,7 @@ var greenScreenImage;
 var bgScreenImage;
 var canvas1;
 var canvas2;
+var canvasFinal;
 
 function upload1() {
     canvas1 = document.getElementById("greenInputCanvas");
@@ -15,4 +16,28 @@ function upload2() {
     var bgScreenInput = document.getElementById("bgImageInput");
     bgScreenImage = new SimpleImage(bgScreenInput);
     bgScreenImage.drawTo(canvas2);
+}
+
+function mergeImage(){
+    canvasFinal=document.getElementById("mergedImage");
+    var image1 = greenScreenImage;
+    var image2 = bgScreenImage;
+    var finalImage = new SimpleImage(image1.width(),image1.height());
+
+
+    for(pixel of image1.values()){
+        if(pixel.getGreen() > (pixel.getRed()+pixel.getBlue())){
+            var x = pixel.getX();
+            var y = pixel.getY();
+            var newPixel = image2.getPixel(x,y);
+            finalImage.setPixel(x,y,newPixel);
+        }
+        else{
+            finalImage.setPixel(pixel.getX(),pixel.getY(),pixel);
+        }
+    }
+    canvasFinal.width = finalImage.width();
+    canvasFinal.height = finalImage.height();
+    finalImage.drawTo(canvasFinal);
+
 }
